1a18d25d224ce25a50bc770a9344db153edd23d2
Igor Rafael <yggor14rafa@hotmail.com>
2017-01-11 16:55:50 -0300

    Adding some docs.

--- Prim.c
+++ Prim.c
@@ -5,14 +5,17 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int i, j, n;
-int visited[3000] = {0}, min, mingrafo = 0, grafo[3000][3000];
+#define TAM_MAX 3000 //Número máximo de nós do grafo
+
+int i, j, n; // n => Número de nós; i,j => Iteradores 
+int visited[TAM_MAX] = {0}, min, mingrafo = 0, grafo[TAM_MAX][TAM_MAX];
 unsigned int soma = 0;
 int fim, tempoPrim[10];
 
-int gera();
+void leArq();
+void gera();
 void prim();
-void escreve();
+void escreveArq();
 
 void main()
 {
@@ -21,7 +24,9 @@ void main()
 	int choice;
 	printf("\nQuantidade de nos: ");
 	scanf("%d", &n);
-	printf("\n1. Para entrar com uma matriz\n2. Para gerar 10 grafos aleatorios\n");
+	printf("\n1. Para entrar com uma matriz");
+	printf("\n2. Para gerar 10 grafos aleatorios");
+	printf("\n3.Ler matriz do arquivo\n-> ");
 	scanf("%d", &choice);
 	if(choice == 1)
 	{
@@ -35,34 +40,41 @@ void main()
 					grafo[i][j] = 999;
 			}
 	}
+	
+	if(choice == 3){
+		leArq();
+		prim(0);
+		return;
+    }
 
 	int iter;
 	for(iter = 0; iter < 10; iter++)
 	{
-		if(choice == 2)gera(iter);
+		if(choice == 2)
+			gera(iter);
 		else
 		{
 			prim(0);
 			break;
 		}
 	}
-	escreve();
+	escreveArq();
 }
 
 
-int gera(int k)
+void gera(int k)
 {
 
-	//GERA O GRAFO ALEATORIO
+	//GERA O GRAFO ALEATÓRIO
 	for(i = 0; i < n; i++)
 		for(j = 0; j < n; j++)
 		{
 			grafo[i][j] = rand() % 20;
-			if(grafo[i][j] < 1)grafo[i][j] = 9999; //se for menor que 400, considera-se que nao ha aresta (custo infinito)
+			if(grafo[i][j] < 1)grafo[i][j] = 9999; //se for menor que 400, considera-se que não há aresta (custo infinito)
 			grafo[j][i] = grafo[i][j];
 		}
 
-	//PRINTS OCULTOS PARA AGILIZAR A EXECUCAO
+	//PRINTS OCULTOS PARA AGILIZAR A EXECUCÃO
 	printf("\nMATRIZ DE ADJACENCIA %d\n", k + 1);
 	for(i = 0; i < n; i++)
 	{
@@ -71,11 +83,11 @@ int gera(int k)
 			if(grafo[i][j] == 9999) printf("0 ");
 			else printf("%d ", grafo[i][j]);
 	}
-	printf("%\n");
+	printf("\n");
 
 	prim(k);
 }
-//encontra o vertice com menor valor de chave dentre os vertices ainda nao incluidos
+//encontra o vértice com menor valor de chave dentre os vértices ainda não incluídos
 int chaveMinima(int chave[], bool arvore[])
 {
 	int min = 9999, min_index;
@@ -90,32 +102,32 @@ int chaveMinima(int chave[], bool arvore[])
 void prim(int iter)
 {
 	int inicio = clock();
-	int visited[n]; //armazena a arvore construida(vertices visitados)
+	int visited[n]; //armazena a árvore construída(vértices visitados)
 	int chave[n]; //valores chave para pegar a aresta de menor custo
-	bool arvore[n]; //vertices ainda nao incluidos na arvore
+	bool arvore[n]; //vértices ainda não incluídos na árvore
 
 	int i;
 	for(i = 0; i < n; i++)
 	{
-		chave[i] = 9999; //todas as chaves sao infinitas inicialmente
+		chave[i] = 9999; //todas as chaves são infinitas inicialmente
 		arvore[i] = false; //nenhum vertice visitado
 	}
-	chave[0] = 0; //primeiro vertice a ser pego: [vertice 1]
-	visited[0] = -1; //primeiro vertice eh a raiz
+	chave[0] = 0; //primeiro vértice a ser pego: [vertice 1]
+	visited[0] = -1; //primeiro vértice é a raiz
 
-	int count; //a arvore vai ter n vertices, e n-1 arestas
+	int count; //a árvore vai ter n vértices, e n-1 arestas
 	for(count = 0; count < (n); count++)
 	{
-		//pega um vertice ainda nao visitado cuja aresta eh minima
+		//pega um vértice ainda não visitado cuja aresta é mínima
 		int u = chaveMinima(chave, arvore);
 
-		//coloca o vertice como visitado
+		//coloca o vértice como visitado
 		arvore[u] = true;
 
 		int v;
 		for(v = 0; v < n; v++)
 		{
-			//se for adjacente e ainda nao estiver incluido, e for menor que a chave, atualize a chave
+			//se for adjacente e ainda nao estiver incluído, e for menor que a chave, atualize a chave
 			if (grafo[u][v] && arvore[v] == false && grafo[u][v] <  chave[v])
 				visited[v]  = u, chave[v] = grafo[u][v];
 		}
@@ -133,10 +145,10 @@ void prim(int iter)
 
 	getch();
 
-	escreve();
+	escreveArq();
 }
 
-void escreve()
+void escreveArq()
 {
 	int total = 0;
 	FILE *fp;
@@ -153,4 +165,20 @@ void escreve()
 		fprintf(fp, "%d ", tempoPrim[i]);
 	}
 	fprintf(fp, "\nSoma Prim: %d", total);
+}
+
+//Lê matriz de adjacência do grafo
+void leArq(){
+	FILE *entrada_grafo;
+	entrada_grafo = fopen("Matriz_grafo.txt", "r");
+	if(!entrada_grafo){
+		printf("Erro na abertura do arquivo Matriz_grafo");
+		exit(1);
+	}
+	
+	for(i = 0; i < n; i++)
+		for(j = 0; j < n; j++)
+			fscanf(entrada_grafo, "%d", &grafo[i][j]);
+		
+	fclose(entrada_grafo);	
 }
